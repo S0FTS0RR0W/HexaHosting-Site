7511376d22a752ee762ca107e9a28fd7f5bff513
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-03-09 11:45:31 +0000

    inital commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Button from '@/components/Button'
+import Link from 'next/link'
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1 className="text-4xl font-bold">Welcome to HexaHosting!</h1>
+      <Link href="/about">
+        <Button>Learn More</Button>
+      </Link>
+    </main>
+  )
+}
