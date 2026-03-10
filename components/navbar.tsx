1782a970af143fec17a74fd688202b8e8cdaaca5
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-03-10 14:19:26 +0000

    Chore: code optimisation

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,7 +12,7 @@ import { ModeToggle } from "./ui/mode-toggle";
 
 export function Navbar() {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container flex h-14 items-center">
         <NavigationMenu>
           <NavigationMenuList>
