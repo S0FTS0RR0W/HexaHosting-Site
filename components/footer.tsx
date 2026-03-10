9c7251b3986ea837cf5a41b03287ea3aa8819043
S0FTS0RR0W <charlie@s0fts0rr0w.com>
2026-03-10 14:31:36 +0000

    Feat: update page style to fit theme and updated footer theme

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     // make the footer stick to the bottom of the viewport and span full width
-    <footer className="fixed bottom-0 w-full z-10 border-t bg-background">
+    <footer className="fixed bottom-0 w-full z-10 border-t bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="container flex flex-col items-center justify-between gap-4 py-4 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose text-muted-foreground md:text-center">
